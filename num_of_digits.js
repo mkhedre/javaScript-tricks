25effbc223282fc7000e843c280b63ed32a15778
mkhedre <mkhedre3@gmail.com>
2022-07-21 14:12:49 +0200

    num_of_digits 3 solutions

--- num_of_digits.js
+++ num_of_digits.js
@@ -1,3 +1,18 @@
 const num_of_digits = (num) => {
   return num.toString().length;
 };
+
+const num_of_digits2 = (num) => {
+  let n = Math.abs(num);
+  let count = 1;
+  while ((n /= 10) >= 1) {
+    count++;
+  }
+  return count;
+};
+console.log(num_of_digits2(123));
+
+const num_of_digits3 = (num) => {
+  return `${num}`.match(/\d/g).length;
+};
+console.log(num_of_digits3(123));
