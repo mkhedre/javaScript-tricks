ce5a7349f26f67c7a2f5b4dd7121d4c3894b9772
mkhedre <mkhedre3@gmail.com>
2022-07-21 13:40:20 +0200

    another solution without conditional operatoor

--- perimeter.js
+++ perimeter.js
@@ -3,4 +3,8 @@
 // c -> circle
 // Perimeter of a square: 4 * side.
 // Perimeter of a circle: 6.28 * radius.
-const perimeter = (l, num) => (l === 's' ? 4 * num : 6.28 * num);
+const perimeter0 = (l, num) => (l === 's' ? 4 * num : 6.28 * num);
+
+const perimeter = (l, num) => {
+  return (4 * (l == 's') + 6.28 * (l == 'c')) * num;
+};
