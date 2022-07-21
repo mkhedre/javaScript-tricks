42e88e7957155160ac61bf97de1f5149b14d1418
mkhedre <mkhedre3@gmail.com>
2022-07-21 13:36:44 +0200

    Perimeter

---
+++ perimeter.js
@@ -0,0 +1,6 @@
+//letter l, number num)
+// s -> square
+// c -> circle
+// Perimeter of a square: 4 * side.
+// Perimeter of a circle: 6.28 * radius.
+const perimeter = (l, num) => (l === 's' ? 4 * num : 6.28 * num);
