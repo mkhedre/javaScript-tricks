8f761c77350f637880fe01e5818c7fb886388027
mkhedre <mkhedre3@gmail.com>
2022-07-23 10:42:43 +0200

    2th sol

--- BurglarySeries.js
+++ BurglarySeries.js
@@ -2,4 +2,7 @@ function addName(obj, name, value) {
   obj[name] = value;
   return obj;
 }
+function addNAme2(obj, name, value) {
+  return { ...obj, [name]: value };
+}
 console.log(addName({ piano: 500 }, 'Brutus', 300));
