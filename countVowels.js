7ff52aaab0d6b4227b4bdfdf0286f84fedda8873
mostafa khedre <moustafa.khedre@rowaad.net>
2023-10-10 04:00:59 +0300

    countVowels

--- countVowels.js
+++ countVowels.js
@@ -11,3 +11,16 @@ function countVowels(text) {
   return counter;
 }
 console.log(countVowels('Hello'));
+
+function countVowels(text) {
+  let count = 0;
+  for (let char of text.toLowerCase()) {
+    count += Number('aeiou'.includes(char));
+  }
+  return count;
+}
+
+function countVowels(text) {
+  // your code here
+  return text.split(/[aeiou]/i).length - 1;
+}
--- longestWord.js
+++ longestWord.js
@@ -13,9 +13,51 @@ function longestWord(sentence) {
       maxIndex = NumKey;
     }
   }
-  console.log(maxIndex);
-  // your code here
   return arr[maxIndex];
 }
 
 console.log(longestWord('hello world'));
+
+// another solution
+
+function longestWord(sentence) {
+  let arr = sentence.split(' ');
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > max.length) max = arr[i];
+  }
+  return max;
+}
+
+// another solution
+
+function longestWord(sentence) {
+  let sortedByLength = sentence.split(' ').sort((a, b) => b.length - a.length);
+  return sortedByLength[0] || '';
+}
+// another solution
+
+function longestWord(sentence) {
+  const result = sentence.split(' ').reduce((acc, cur) => {
+    if (cur.length > acc.length) {
+      acc = cur;
+    }
+    return acc;
+  }, '');
+  return result;
+}
+/**
+ * reduce((acc, cur) => {...}, ''): The reduce function is used to iterate over the array of words. It takes two parameters:
+ * acc: The accumulator, which starts as an empty string ('').
+ * cur: The current element being processed, which represents a word from the sentence.
+ */
+
+function longestWord(sentence) {
+  if (!sentence) return '';
+  return sentence
+    .match(/\w+/g)
+    .reduce(
+      (longest, word) => (word.length > longest.length ? word : longest),
+      ''
+    );
+}
